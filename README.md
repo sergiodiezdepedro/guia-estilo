a2c0315b5d7b00f1b328acf3a54f1400ff309429
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2018-05-10 23:53:37 +0200

    Proyecto iniciado con cuatro páginas html

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guía de estilo
+
+Una guía de estilo hipotética, en la que voy incluyendo componentes y elementos que me gustan.
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+    purify = require('gulp-purifycss');
+
+gulp.task('serve', ['sass'], function () {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src("scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions']
+        }))
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+gulp.task('cssprod', function () {
+    return gulp.src("css/*.css")
+        .pipe(purify(['./js/*.js', './*.html'], {
+            minify: true
+        }))
+        .pipe(gulp.dest("css"));
+});
